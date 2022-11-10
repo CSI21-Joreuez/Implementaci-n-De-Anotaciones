b99570da0a536a9a439b21e5a41281366fe9e358
Juan Carlos Orea Rodríguez <csi21-joreuez@colegioaltair.net>
2022-11-10 21:49:46 +0100

    Terminado

---
+++ src/main/java/com/app/anota/dao/UsuarioImplMongo.java
@@ -0,0 +1,17 @@
+package com.app.anota.dao;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioImplMongo implements UsuarioService {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void inserta() {
+		System.out.println("Insertando Usuario con Mongo");
+	}
+
+
+}
---
+++ src/main/java/com/app/anota/dao/UsuarioImplOracle.java
@@ -0,0 +1,19 @@
+package com.app.anota.dao;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioImplOracle implements UsuarioService {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+	}
+
+	public void inserta() {
+		System.out.println("Insertando Usuario con Oracle");
+	}
+
+}
